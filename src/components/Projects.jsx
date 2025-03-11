a15cb1295cab3d740020497e1296e8a229ae34b8
antekazq <antonio@qajar.se>
2025-03-11 19:46:19 +0100

    Added Game Projects

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -5,6 +5,7 @@ import { faGithub, faLinkedin } from "@fortawesome/free-brands-svg-icons";
 
 
 
+
 const Projects = () => {
     return (
         <section id = "projects" className="projects-container">
@@ -15,13 +16,30 @@ const Projects = () => {
                 PubMed API that saves data in a PostgreSQL database that I set up. Feel free to try a search
                 term <a href="https://pubmedapi-0fl5.onrender.com" target="_blank" rel="noopener noreferrer">
                 <u>here</u></a> </p>
-            
+
             <h2 className="adventofcode-project">Advent of Code 2024</h2>
             <p> I challenge myself to develop my Python skills by efficiently solving
                 programming problems from Advent of Code. I continuously post my solutions and
                 documentation on GitHub, which can be found <a href="https://github.com/antekazq/AdventOfCode" target="_blank" rel="noopener noreferrer">
                 <u>here</u></a>
             </p>
+
+            <h2 className="donkeykong-project">Donkey Kong Arcade</h2>
+            <p>I developed this recreation of the classic Donkey Kong arcade game using C# and MonoGame, 
+                applying object-oriented programming principles. Check out a sample video below!</p>
+            <video className="project-video" controls>
+                <source src="/videos/DonkeyKong.mp4" type="video/mp4" />
+                Your browser does not support the video tag.
+            </video>
+
+            <h2 className="pacman-project">Pac-Man</h2>
+            <p>I built this Pac-Man remake in C# and MonoGame with an OOP-based structure, featuring tile-based movement,
+                randomized ghost behavior, and frame-based animations. Check out a sample video below!</p>
+            <video className="project-video" controls>
+                <source src="/videos/PacManFinal.mp4" type="video/mp4" />
+                Your browser does not support the video tag.
+            </video>
+
             <div className="projects-footer">
                 <p>If you’d like to view more of my smaller projects and follow my updates, you can visit my GitHub and LinkedIn:</p>
                 <div className="social-links">
